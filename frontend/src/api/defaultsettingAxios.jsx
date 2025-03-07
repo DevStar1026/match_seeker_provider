25b9e3c3f38126dc5948c0a4b0d3765679334665
DevStar1026 <batuba1107@gmail.com>
2025-03-08 00:04:22 +0800

    change the .env file

--- frontend/src/api/defaultsettingAxios.jsx
+++ frontend/src/api/defaultsettingAxios.jsx
@@ -1,4 +1,4 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://127.0.0.1:5000/";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 export default axios;
